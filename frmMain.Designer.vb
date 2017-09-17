356c0514f6d233a0e33755fbee279238bb87d5bb
dunkan50 <dunkim2008@gmail.com>
2017-09-17 17:03:19 +0300

    First Commit

---
+++ frmMain.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tblouter = New System.Windows.Forms.TableLayoutPanel()
+        Me.trainingButton = New System.Windows.Forms.Button()
+        Me.choosenFile = New System.Windows.Forms.Label()
+        Me.tbInfo = New System.Windows.Forms.TextBox()
+        Me.ofDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.tblouter.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tblouter
+        '
+        Me.tblouter.ColumnCount = 2
+        Me.tblouter.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblouter.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tblouter.Controls.Add(Me.trainingButton, 0, 0)
+        Me.tblouter.Controls.Add(Me.choosenFile, 1, 0)
+        Me.tblouter.Controls.Add(Me.tbInfo, 0, 1)
+        Me.tblouter.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tblouter.Location = New System.Drawing.Point(0, 0)
+        Me.tblouter.Name = "tblouter"
+        Me.tblouter.RowCount = 2
+        Me.tblouter.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblouter.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tblouter.Size = New System.Drawing.Size(867, 497)
+        Me.tblouter.TabIndex = 0
+        '
+        'trainingButton
+        '
+        Me.trainingButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.trainingButton.AutoSize = True
+        Me.trainingButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.trainingButton.Font = New System.Drawing.Font("Old English Text MT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.trainingButton.Location = New System.Drawing.Point(3, 3)
+        Me.trainingButton.Name = "trainingButton"
+        Me.trainingButton.Size = New System.Drawing.Size(100, 30)
+        Me.trainingButton.TabIndex = 0
+        Me.trainingButton.Text = "Open Image"
+        Me.trainingButton.UseVisualStyleBackColor = True
+        '
+        'choosenFile
+        '
+        Me.choosenFile.AutoSize = True
+        Me.choosenFile.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.choosenFile.Font = New System.Drawing.Font("Old English Text MT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.choosenFile.Location = New System.Drawing.Point(109, 0)
+        Me.choosenFile.Name = "choosenFile"
+        Me.choosenFile.Size = New System.Drawing.Size(755, 36)
+        Me.choosenFile.TabIndex = 1
+        Me.choosenFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'tbInfo
+        '
+        Me.tblouter.SetColumnSpan(Me.tbInfo, 2)
+        Me.tbInfo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbInfo.Font = New System.Drawing.Font("Old English Text MT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbInfo.Location = New System.Drawing.Point(3, 39)
+        Me.tbInfo.Multiline = True
+        Me.tbInfo.Name = "tbInfo"
+        Me.tbInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.tbInfo.Size = New System.Drawing.Size(861, 455)
+        Me.tbInfo.TabIndex = 2
+        Me.tbInfo.WordWrap = False
+        '
+        'ofDialog
+        '
+        Me.ofDialog.FileName = "OpenFileDialog1"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(867, 497)
+        Me.Controls.Add(Me.tblouter)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.tblouter.ResumeLayout(False)
+        Me.tblouter.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tblouter As TableLayoutPanel
+    Friend WithEvents trainingButton As Button
+    Friend WithEvents choosenFile As Label
+    Friend WithEvents tbInfo As TextBox
+    Friend WithEvents ofDialog As OpenFileDialog
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,166 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.Xml
+Imports System.Xml.Serialization 'this is fro the imports seen at the end of the program
+Imports System.IO
+
+Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.CV.UI
+Imports Emgu.CV.Util
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+Public Class frmMain
+    '''''MAIN VARIABLES''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Const MIN_CONTOUR_AREA As Integer = 100
+    Const CHANGED_IMAGE_WIDTH As Integer = 20
+    Const CHANGED_IMAGE_HEIGHT As Integer = 30
+
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    Private Sub trainingButton_Click(sender As Object, e As EventArgs) Handles trainingButton.Click
+        Dim getChoosenFile As DialogResult
+        getChoosenFile = ofDialog.ShowDialog()
+        If (getChoosenFile <> DialogResult.OK Or ofDialog.FileName = "") Then  'if user chose cancle or filename is blank...
+            choosenFile.Text = "file not opened"
+            Return
+        End If
+
+        Dim numberTraingingImage As Mat
+
+        Try
+            numberTraingingImage = CvInvoke.Imread(ofDialog.FileName, LoadImageType.Color)
+        Catch ex As Exception
+            'when there are issues with the image
+            choosenFile.Text = "Not able to open image" + ex.Message  'will show the error message from the error
+            Return
+        End Try
+
+        If (numberTraingingImage Is Nothing) Then
+            choosenFile.Text = "unable to open the file"
+            Return
+
+        End If
+
+        choosenFile.Text = ofDialog.FileName 'update fro the file label
+
+        Dim grayscaleImage As New Mat()
+        Dim blurredImage As New Mat()
+        Dim threshImage As New Mat()
+        Dim copyOfThreshImage As New Mat()
+        Dim contours As New VectorOfVectorOfPoint()
+
+        Dim classificationMatrix As Matrix(Of Single)
+        Dim trainingImagesMatrix As Matrix(Of Single)
+
+        Dim flattenedTrainingImages As New Mat()
+
+        Dim intValidChars As New List(Of Integer)(New Integer() {Asc("0"), Asc("1"), Asc("2"), Asc("3"), Asc("4"), Asc("5"), Asc("6"), Asc("7"), Asc("8"), Asc("9"),
+                                                                  Asc("A"), Asc("B"), Asc("C"), Asc("D"), Asc("E"), Asc("F"), Asc("G"), Asc("H"), Asc("I"), Asc("J"),
+                                                                  Asc("K"), Asc("L"), Asc("M"), Asc("N"), Asc("O"), Asc("P"), Asc("Q"), Asc("R"), Asc("S"), Asc("T"),
+                                                                  Asc("U"), Asc("V"), Asc("W"), Asc("X"), Asc("Y"), Asc("Z")})
+        CvInvoke.CvtColor(numberTraingingImage, grayscaleImage, ColorConversion.Bgr2Gray) 'grayscale conversion
+        CvInvoke.GaussianBlur(grayscaleImage, blurredImage, New Size(5, 5), 0) 'blurring the given image
+        CvInvoke.AdaptiveThreshold(blurredImage, threshImage, 255.0, AdaptiveThresholdType.GaussianC, ThresholdType.BinaryInv, 11, 2) 'converting the image to its threshhold image
+        CvInvoke.Imshow("Thresh image", threshImage) 'shows the image being processed the thresh image
+
+        copyOfThreshImage = threshImage.Clone() 'copy of the the image
+
+        CvInvoke.FindContours(copyOfThreshImage, contours, Nothing, RetrType.External, ChainApproxMethod.ChainApproxSimple) 'gets only the external contours only
+
+        Dim numberofTrainingSamples As Integer = contours.Size
+
+        classificationMatrix = New Matrix(Of Single)(numberofTrainingSamples, 1) 'this is the classification of the data structure
+        trainingImagesMatrix = New Matrix(Of Single)(numberofTrainingSamples, CHANGED_IMAGE_WIDTH * CHANGED_IMAGE_HEIGHT) 'keeps track of the roow we are in as the data is being analysied
+
+        Dim trainingdataRowTOAdd As Integer = 0 'the smple will coresspond to a row in both the classificasion xml and the traingng images xml files
+
+        For i As Integer = 0 To contours.Size - 1
+            If (CvInvoke.ContourArea(contours(i)) > MIN_CONTOUR_AREA) Then
+                Dim boundingrect As Rectangle = CvInvoke.BoundingRectangle(contours(i)) 'to get the charaters bounding rectangle
+                CvInvoke.Rectangle(numberTraingingImage, boundingrect, New MCvScalar(0.0, 0.0, 255.0), 2) 'draws a red rect around each contour
+
+                Dim copyofROIImage As New Mat(threshImage, boundingrect) 'gets data for the ROI image (Region Of Intrest)
+                Dim ROIImage As Mat = copyofROIImage.Clone 'make a copy of image
+                Dim ROIImageResized As New Mat() 'this is to alter the area of the image gotten
+
+                CvInvoke.Resize(ROIImage, ROIImageResized, New Size(CHANGED_IMAGE_WIDTH, CHANGED_IMAGE_HEIGHT)) 'changed the height and width of the image
+                CvInvoke.Imshow("ROI", ROIImage)
+                CvInvoke.Imshow("ROI resized", ROIImageResized)
+                CvInvoke.Imshow("Training images", numberTraingingImage) 'shows the image of the trainig images with the roi marked
+
+                Dim charsInt As Integer = CvInvoke.WaitKey(0) 'pick up any key press
+                If (charsInt = 27) Then
+                    CvInvoke.DestroyAllWindows()
+                    Return 'if the esc ket was pressed the program will exit
+
+                ElseIf (intValidChars.Contains(charsInt)) Then
+                    classificationMatrix(trainingdataRowTOAdd, 0) = Convert.ToSingle(charsInt) 'writes the classification chars to classifaction matrixes
+                    'now we can add the training matrixes but we need to convert them allitle
+
+                    Dim tempMatrix As Matrix(Of Single) = New Matrix(Of Single)(ROIImageResized.Size())
+                    Dim reshapedTempMatrix As Matrix(Of Single) = New Matrix(Of Single)(1, CHANGED_IMAGE_WIDTH * CHANGED_IMAGE_HEIGHT) 'does the conversion
+
+                    ROIImageResized.ConvertTo(tempMatrix, DepthType.Cv32F) 'changes the matrix image to singles of the same
+
+                    For intRow As Integer = 0 To CHANGED_IMAGE_HEIGHT - 1 'flatten Matrix into one row by RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT number of columns
+                        For intColumn As Integer = 0 To CHANGED_IMAGE_WIDTH - 1
+                            reshapedTempMatrix(0, (intRow * CHANGED_IMAGE_WIDTH) + intColumn) = tempMatrix(intRow, intColumn)
+                        Next
+                    Next
+
+                    For intColumn As Integer = 0 To (CHANGED_IMAGE_WIDTH * CHANGED_IMAGE_HEIGHT) - 1
+                        trainingImagesMatrix(trainingdataRowTOAdd, intColumn) = reshapedTempMatrix(0, intColumn)
+
+                    Next
+                    trainingdataRowTOAdd = trainingdataRowTOAdd + 1 'increases the column by one in the loop
+
+                End If
+
+            End If
+        Next
+
+        tbInfo.Text = tbInfo.Text + "finished the training " + vbCrLf + vbCrLf 'saves the classification to a file
+
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        Dim xmlserial As XmlSerializer = New XmlSerializer(classificationMatrix.GetType)
+        Dim writer As StreamWriter
+
+        Try
+            writer = New StreamWriter("classification.xml") 'this tries to open the classification files
+        Catch ex As Exception
+            tbInfo.Text = vbCrLf + tbInfo.Text + "unanble to open the classificaton file , error" + vbCrLf
+            tbInfo.Text = tbInfo.Text + ex.Message + vbCrLf + vbCrLf
+
+            Return
+
+        End Try
+
+        xmlserial.Serialize(writer, classificationMatrix) 'this is to save the training images
+        writer.Close()
+
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        xmlserial = New XmlSerializer(trainingImagesMatrix.GetType)
+
+        Try
+            writer = New StreamWriter("images.xml") 'tries to open the images file
+        Catch ex As Exception
+            tbInfo.Text = vbCrLf + tbInfo.Text + "Not able to open file, error:" + vbCrLf
+            tbInfo.Text = tbInfo.Text + ex.Message + vbCrLf + vbCrLf
+            Return
+        End Try
+
+        xmlserial.Serialize(writer, trainingImagesMatrix)
+        writer.Close()
+
+        tbInfo.Text = vbCrLf + tbInfo.Text + "file writing done" + vbCrLf
+
+        MsgBox("trainging complete, file writing too!!!")
+
+
+    End Sub
+End Class
